5f058e2d10f66bcb4e2ede4f0776639751b8886a
mohamed <mohamedguiliano@hotmail.fr>
2023-09-20 02:19:36 +0200

    mise a jour du code

--- admin/index.php
+++ admin/index.php
@@ -1,57 +1,75 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>MY BURGER</title>
+        <title>Burger Code</title>
         <meta charset="utf-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
         <link href='http://fonts.googleapis.com/css?family=Holtwood+One+SC' rel='stylesheet' type='text/css'>
-        <link rel="stylesheet" href="../css/styles.css">
+        <link rel="stylesheet" href="css/styles.css">
     </head>
     
+    
     <body>
-        <h1 class="text-logo"><span class="glyphicon glyphicon-cutlery"></span> Burger Code <span class="glyphicon glyphicon-cutlery"></span></h1>
-        <div class="container admin">
-            <div class="row">
-                <h1><strong>Liste des items   </strong><a href="insert.php" class="btn btn-success btn-lg"><span class="glyphicon glyphicon-plus"></span> Ajouter</a></h1>
-                <table class="table table-striped table-bordered">
-                  <thead>
-                    <tr>
-                      <th>Nom</th>
-                      <th>Description</th>
-                      <th>Prix</th>
-                      <th>Catégorie</th>
-                      <th>Actions</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                      <?php
-                        require 'database.php';
-                        $db = Database::connect();
-                        $statement = $db->query('SELECT items.id, items.name, items.description, items.price, categories.name AS category FROM items LEFT JOIN categories ON items.category = categories.id ORDER BY items.id DESC');
-                        while($item = $statement->fetch()) 
-                        {
-                            echo '<tr>
-							<td>'. $item['name'] . '</td>
-							<td>'. $item['description'] . '</td>
-							<td>'. number_format($item['price'], 2, '.', '') . '</td>
-							<td>'. $item['category'] . '</td>
-							<td width=300>
-							<a class="btn btn-default" href="view.php?id='.$item['id'].'"><span class="glyphicon glyphicon-eye-open"></span> Voir</a>&nbsp;';
-//                            echo ' ';
-                            echo '<a class="btn btn-primary" href="update.php?id='.$item['id'].'"><span class="glyphicon glyphicon-pencil"></span> Modifier</a>&nbsp;';
-//                            echo ' ';
-                            echo '<a class="btn btn-danger" href="delete.php?id='.$item['id'].'"><span class="glyphicon glyphicon-remove"></span> Supprimer</a>';
-                            echo     '</td>';
-                            echo '</tr>';
-                        }
-                        Database::disconnect();
-                      ?>
-                  </tbody>
-                </table>
-            </div>
+        <div class="container site">
+            <h1 class="text-logo"><span class="glyphicon glyphicon-cutlery"></span> Burger Code <span class="glyphicon glyphicon-cutlery"></span></h1>
+            <?php
+				require 'admin/database.php';
+			 
+                echo '<nav>
+                        <ul class="nav nav-pills">';
+
+                $db = Database::connect();
+                $statement = $db->query('SELECT * FROM categories');
+                $categories = $statement->fetchAll();
+                foreach ($categories as $category) 
+                {
+                    if($category['id'] == '1')
+                        echo '<li role="presentation" class="active"><a href="#'. $category['id'] . '" data-toggle="tab">' . $category['name'] . '</a></li>';
+                    else
+                        echo '<li role="presentation"><a href="#'. $category['id'] . '" data-toggle="tab">' . $category['name'] . '</a></li>';
+                }
+
+                echo    '</ul>
+                      </nav>';
+
+                echo '<div class="tab-content">';
+
+                foreach ($categories as $category) 
+                {
+                    if($category['id'] == '1')
+                        echo '<div class="tab-pane active" id="' . $category['id'] .'">';
+                    else
+                        echo '<div class="tab-pane" id="' . $category['id'] .'">';
+                    
+                    echo '<div class="row">';
+                    
+                    $statement = $db->prepare('SELECT * FROM items WHERE items.category = ?');
+                    $statement->execute(array($category['id']));
+                    while ($item = $statement->fetch()) 
+                    {
+                        echo '<div class="col-sm-6 col-md-4">
+                                <div class="thumbnail">
+                                    <img src="images/' . $item['image'] . '" alt="...">
+                                    <div class="price">' . number_format($item['price'], 2, '.', ''). ' €</div>
+                                    <div class="caption">
+                                        <h4>' . $item['name'] . '</h4>
+                                        <p>' . $item['description'] . '</p>
+                                        <a href="#" class="btn btn-order" role="button"><span class="glyphicon glyphicon-shopping-cart"></span> Commander</a>
+                                    </div>
+                                </div>
+                            </div>';
+                    }
+                   
+                   echo    '</div>
+                        </div>';
+                }
+                Database::disconnect();
+                echo  '</div>';
+            ?>
         </div>
     </body>
 </html>
+
